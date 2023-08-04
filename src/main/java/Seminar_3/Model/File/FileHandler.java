3102b838f8761c222b35915b505ca7f2a0997e10
Daria <shkatov.pad@gmail.com>
2023-08-04 18:49:12 +0300

    ere

--- src/main/java/Seminar_3/Model/File/FileHandler.java
+++ src/main/java/Seminar_3/Model/File/FileHandler.java
@@ -20,15 +20,6 @@ public class FileHandler implements Workable {
 
     @Override
     public void fileR(String path) {
-//        try (FileReader fileReader = new FileReader(path)) {
-//            int ch = fileReader.read();
-//            while (ch != -1) {
-//                System.out.print((char) ch);
-//                fileReader.close();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
             String line = bufferedReader.readLine();
             while (line != null) {
